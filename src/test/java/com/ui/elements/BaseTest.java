f040fd92f5a8ee809b2958808eaaa0dab3e93e72
AnnadTenneti <tenneti.anand@gmail.com>
2024-11-27 20:51:05 +0530

    Adding BaseTest.java file

--- src/test/java/com/ui/elements/BaseTest.java
+++ src/test/java/com/ui/elements/BaseTest.java
@@ -1,22 +1,64 @@
+package com.ui.elements;
+
+import com.utils.Utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+
+import javax.swing.text.DateFormatter;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class BaseTest {
-    WebDriver driver;
+    public static WebDriver driver;
+    public static String screenshotsSubFolderName;
 
-    @BeforeMethod
+    @BeforeTest
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.get("http://uitestingplayground.com/");
 
     }
 
+    @BeforeMethod
+    public void launchURL() {
+        driver.get("http://uitestingplayground.com/");
+    }
+
     @AfterMethod
+    public void screenShotCapture(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            CaptureScreenshot(result.getTestContext().getName() + "_" + result.getName());
+        }
+    }
+
+    @AfterTest
     public void tearDown() {
         driver.quit();
+    }
+
+    public static void CaptureScreenshot(String fileName) {
+        if (screenshotsSubFolderName == null) {
+            LocalDateTime dateObj = LocalDateTime.now();
+            DateTimeFormatter formatObj = DateTimeFormatter.ofPattern("ddMMyyyyHHmmss");
+            screenshotsSubFolderName = dateObj.format(formatObj);
+            System.out.println(screenshotsSubFolderName);
+        }
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File file = ts.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file,
+                    new File("./Screenshots/" + screenshotsSubFolderName + "/" + fileName+".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Screenshot Saved successfully");
     }
 }
