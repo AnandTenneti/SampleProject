d578b17bbb8f4e4abf2a0bc343771a12e336d96e
AnnadTenneti <tenneti.anand@gmail.com>
2024-12-16 17:22:21 +0530

    Updated the DataSupplier.java file with data generated using Faker class

--- src/test/java/com/dataprovider/DataSupplier.java
+++ src/test/java/com/dataprovider/DataSupplier.java
@@ -1,14 +1,16 @@
 package com.dataprovider;
 
+import com.github.javafaker.Faker;
 import org.testng.annotations.DataProvider;
 
 public class DataSupplier {
+    Faker faker = new Faker();
 
     @DataProvider(name = "loginTestData")
     public String[] loginData() {
         String[] data = new String[2];
-        data[0] = "Admin";
-        data[1] = "Admin1";
+        data[0] = faker.name().fullName();
+        data[1] = faker.name().fullName();
 
         return data;
     }
